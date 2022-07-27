c362762d84d83d9510b600422f7e8ade4148200c
Rustamkhonov Mukhammad-Rakhimkhon <m.rustamkhonov@student.inha.uz>
2022-07-28 02:28:37 +0500

    seela_brad

---
+++ master_app/lib/app/ui/pages/choose_role_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChooseRolePage extends StatelessWidget {
+  const ChooseRolePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
