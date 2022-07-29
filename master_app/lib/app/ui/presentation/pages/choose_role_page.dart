0c198ce517889baf1edefe2251014e1b42d5881a
Rustamkhonov Mukhammad-Rakhimkhon <m.rustamkhonov@student.inha.uz>
2022-07-29 13:32:24 +0500

    feat:added the page for choosing role

--- master_app/lib/app/ui/presentation/pages/choose_role_page.dart
+++ master_app/lib/app/ui/presentation/pages/choose_role_page.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
 
 class ChooseRolePage extends StatelessWidget {
-  const ChooseRolePage({Key? key}) : super(key: key);
+  const ChooseRolePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      appBar: CurvedAppBar(),
+    );
   }
 }
+
+class CurvedAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CurvedAppBar({
+    super.key,
+    this.title,
+    this.curvature,
+    this.backgroundColor,
+  });
+  final String? title;
+  final Curvature? curvature;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      bottom: const CurvedBottom(),
+      title: title != null ? Text(title!) : null,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(80);
+}
+
+class CurvedBottom extends StatelessWidget implements PreferredSizeWidget {
+  const CurvedBottom({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(30);
+}
+
+class CurvatureClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    throw UnimplementedError();
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
+
+enum Curvature { lined, leftToRight, rightToLeft }
